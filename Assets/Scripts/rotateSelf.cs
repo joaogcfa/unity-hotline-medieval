dab45d034fe9a1fea76146072991e111669ffb38
joaogcfa <joaogcfa@al.insper.edu.br>
2022-03-21 11:39:08 -0300

    wizard shooting

--- Assets/Scripts/rotateSelf.cs
+++ Assets/Scripts/rotateSelf.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Movement : MonoBehaviour
+public class rotateSelf : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Vector2 mousePos;
+    public Camera cam;
+
     public Vector2 velocidade;
     public float speed = 10.0f;
 
@@ -26,5 +29,9 @@ public class Player_Movement : MonoBehaviour
         velocidade = new Vector2(inputX*speed, inputY*speed);
         rb.MovePosition(rb.position + velocidade * Time.fixedDeltaTime);
 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
